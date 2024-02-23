136779a289c3a106494b1eda7fdcdad974c38b4e
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2024-02-23 09:49:30 -0800

    L

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-#create file in /temp
-
-file { '/temp/schoo;':
-    ensure => file,
-    owner  => 'www-data',
-    group  => 'www-data',
-    mode   => 0744,
+# Creates a file
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
