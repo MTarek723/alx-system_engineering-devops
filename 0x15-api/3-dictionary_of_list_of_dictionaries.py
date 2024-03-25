8cde323918b0ed185f24cab6c1e1420ed0bf52f6
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2024-03-25 14:55:50 -0700

    L

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Exports to-do list information for a given employee ID to JSON format."""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users/").json()
+
+    with open("all.json", "w") as jsonfile:
+        for user in users:
+            todos = requests.get(
+                url + "todos", params={"userId": user["id"]}).json()
+            for t in todos:
+                jsonfile.write(
+                    json.dumps(
+                        {user["id"]: [{
+                            "username": t["title"],
+                            "task": t["title"],
+                            "completed": t["completed"]
+                        }]}
+                    )
+                )
