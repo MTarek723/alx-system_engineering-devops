83793d33a966edc1ef8bdf0b04dc5b5227de6178
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2024-04-09 05:31:06 -0700

    L

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,8 +7,8 @@ def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "0x16-api_advanced:project:\
-v1.0.0 (by /u/Tefa_23)"
+        "User-Agent": "0x16-apiadvanced:project:\
+v1.0.0"
     }
     params = {
         "limit": 10
