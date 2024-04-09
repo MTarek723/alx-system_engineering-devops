bb96efa644eae8da737454891d14e8f40072cdf1
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2024-04-09 05:10:39 -0700

    L

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""contain subs function """
+"""
+Contains the number_of_subscribers function
+"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """function to return number of subscribers"""
+    """returns the number of subscribers for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    r = requests.get(
-            "https://www.reddit.com/r/{}/about.json".format(subreddit))
-    r = r.json()
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
     subs = r.get("data", {}).get("subscribers", 0)
     return subs
