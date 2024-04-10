2b4518244feec6f51bee082134ec56461259a9c2
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2024-04-10 03:10:50 -0700

    L

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 """ Function that queries the Reddit API """
 import requests
 import sys
@@ -10,10 +9,11 @@ def number_of_subscribers(subreddit):
     Returns:
         number of subscribers to the subreddit,
         or 0 if subreddit requested is invalid"""
-    headers = {'User-Agent': 'itbecomessoannoying 1.0'}
+    headers = {'User-Agent': '(u/Tefa_23)'}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         return (response.json().get("data").get("subscribers"))
     return (0)
+print(number_of_subscribers('programming'))
