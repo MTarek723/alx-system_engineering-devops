eefd9af2b738d8f2dcdca09164b928f258b27e8d
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2024-04-10 04:10:15 -0700

    L

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,13 +7,15 @@ def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "0x16-apiadvanced:project:\
-v1.0.0"
+        "User-Agent": "User-Agent': '5ara/v1.0.0 (by /u/Tefa_23)"
     }
     params = {
         "limit": 10
     }
-    response = requests.get(url, headers=headers, params=params,
+    id = 'Iveor9egCPolZahGK0MPpQ'
+    passw = 'vgxoTcT2eVMFL7THy77MW786eGYAYQ'
+    auth = requests.auth.HTTPBasicAuth(id, passw)
+    response = requests.get(url, headers=headers, params=params,auth=auth,
                             allow_redirects=False)
     if response.status_code == 404:
         print("None")
