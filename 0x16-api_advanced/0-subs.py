6dc6ad3c8a428d36e0c273ccbb8659e8b0893f47
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2024-04-10 04:03:16 -0700

    L

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,18 +4,17 @@ Contains the number_of_subscribers function
 """
 
 import requests
-import json
 
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit"""
     id = 'Iveor9egCPolZahGK0MPpQ'
-    key = 'vgxoTcT2eVMFL7THy77MW786eGYAYQ'
-    auth = requests.auth.HTTPBasicAuth(id,key)
+    passw = 'vgxoTcT2eVMFL7THy77MW786eGYAYQ'
+    auth = requests.auth.HTTPBasicAuth(id, passw)
     if subreddit is None or type(subreddit) is not str:
         return 0
     r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': 'linux:project:/v1.0.0 (by /u/Tefa_23)'},
-                     auth=auth).json()    
+                     headers={'User-Agent': '5ara/v1.0.0 (by /u/Tefa_23)'},
+                     auth=auth).json()
     subs = r.get("data", {}).get("subscribers", 0)
     return subs
