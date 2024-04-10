6c8e8700bb15cd871f75f725a7fcb1029a2099e5
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2024-04-10 02:55:29 -0700

    L

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,39 +1,19 @@
 #!/usr/bin/python3
-
 """ Function that queries the Reddit API """
-
 import requests
 import sys
-import time
-import json
 
 
 def number_of_subscribers(subreddit):
-    """ Args:
-    subreddit: subreddit name
+    """  Args:
+        subreddit: subreddit name
     Returns:
-    number of subscribers to the subreddit,
-    or 0 if subreddit requested is invalid or if there is a JSONDecodeError"""
-
-    headers = {'User-Agent': 'xica369'}
+        number of subscribers to the subreddit,
+        or 0 if subreddit requested is invalid"""
+    headers = {'User-Agent': 'itbecomessoannoying 1.0'}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    max_retries = 5  # Maximum number of retries
-    retry_delay = 3  # Delay between retries in seconds
-
-    for attempt in range(max_retries):
-        try:
-            response = requests.get(
-                url, headers=headers, allow_redirects=False)
-            response.raise_for_status()  # Raise error for non-2xx status codes
-            data = response.json()
-            return data.get("data", {}).get("subscribers", 0)
-        except json.JSONDecodeError:
-            print("Error: Unable to parse JSON response")
-            if attempt == max_retries - 1:
-                return 0
-            time.sleep(retry_delay)  # Wait before retrying
-        except requests.exceptions.RequestException as e:
-            print(f"Error: {e}")
-            return 0
-    return 0
+    if response.status_code == 200:
+        return (response.json().get("data").get("subscribers"))
+    return (0)
