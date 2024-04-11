c3d21611ae82b4e525095513a8c78a6f62e5166c
mahmoud tarek <mahmoud_ta_rek@outlook.com>
2024-04-11 07:55:32 -0700

    L

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+
 """
 Contains the number_of_subscribers function
 """
 
 import requests
-
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit"""
@@ -13,12 +13,18 @@ def number_of_subscribers(subreddit):
     auth = requests.auth.HTTPBasicAuth(id, passw)
     headers = {
         'User-Agent': 'RedditAPIClient/1.0 (https://github.com/MTarek723)'
-               }
+    }
+
     if subreddit is None or type(subreddit) is not str:
         return 0
-    r = requests.get('https://www.reddit.com/r/{}/about.json'
-                     .format(subreddit),
-                     headers=headers,
-                     auth=auth).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+
+    try:
+        r = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
+                         headers=headers,
+                         auth=auth)
+        r.raise_for_status()  # Raise an exception for non-2xx status codes
+        subs = r.json().get("data", {}).get("subscribers", 0)
+        return subs
+    except requests.exceptions.RequestException:
+        # Handle any exceptions that occur during the request
+        return 0
